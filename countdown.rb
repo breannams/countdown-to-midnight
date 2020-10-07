de1ec7844077fafba85a799e0f29af24bd2e35a1
Breanna Scheibelhoffer <breannamarie_s@hotmail.com>
2020-10-06 21:48:36 -0400

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+        return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number_two)
+    while number_two > 0
+        puts "#{number_two} SECOND(S)!"
+        number_two -= 1
+        sleep 1
+    end
+        return "HAPPY NEW YEAR!"
 end
